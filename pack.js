9758c2ada187493fe7cee01ff00a77e3923d884f
acesafff-ship-it <acesafff@gmail.com>
2026-07-22 23:34:09 +0200

    YssPack 0.4.1: płaskie kolory akcji gracza

--- pack.js
+++ pack.js
@@ -4,7 +4,7 @@ if (!host || document.querySelector('#ysspack')) {
   throw new Error('[YssPack] Loader nie jest aktywny albo panel został już uruchomiony.');
 }
 
-const PACK_VERSION = '0.4.0';
+const PACK_VERSION = '0.4.1';
 const STORAGE_PREFIX = 'ysspack_';
 const today = new Date();
 const moduleCacheKey = [today.getFullYear(), String(today.getMonth() + 1).padStart(2, '0'), String(today.getDate()).padStart(2, '0')].join('');
