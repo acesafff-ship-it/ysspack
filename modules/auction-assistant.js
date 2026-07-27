29f40556278d8173934800fa7950ce178a943c36
acesafff-ship-it <acesafff-ship-it@users.noreply.github.com>
2026-07-27 22:03:56 +0200

    Przywrócenie tła wewnątrz ramki

--- modules/auction-assistant.js
+++ modules/auction-assistant.js
@@ -55,7 +55,7 @@ async function loadAndRun() {
 export default {
   id: 'auction-assistant',
   name: 'Asystent Aukcji',
-  version: '2.0.3',
+  version: '2.0.4',
   description: 'Automatycznie pobiera ceny przedmiotu bez otwierania listy aukcji.',
   icon: '⚖',
 
