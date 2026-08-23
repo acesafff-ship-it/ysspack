94cc5406c018fa2fb14cafc11b06fa3c435b7822
acesafff-ship-it <acesafff-ship-it@users.noreply.github.com>
2026-08-23 23:05:38 +0200

    Update Bestiary duplicated neutral group

--- modules/bestiary.js
+++ modules/bestiary.js
@@ -1,5 +1,5 @@
 const MODULE_ID = 'bestiary';
-const SOURCE_URL = 'https://raw.githubusercontent.com/acesafff-ship-it/margohelp-bestiariusz/58c57b3a2449ab97f28238d3fc6bc5b2317faa52/MargoHelp-Bestiariusz.user.js';
+const SOURCE_URL = 'https://raw.githubusercontent.com/acesafff-ship-it/margohelp-bestiariusz/2a79e74171600ef2767bcff9df9f8def5bd96ab8/MargoHelp-Bestiariusz.user.js';
 const GLOBAL_FLAG = '__KROL_YSS_FORUM_ELITE_ITEMS__';
 const PRESENCE_FLAG = '__KROL_YSS_BESTIARY_PRESENCE__';
 const UI_SELECTORS = [
@@ -71,7 +71,7 @@ function executeBestiary(source, context) {
 export default {
   id: MODULE_ID,
   name: 'Bestiariusz Podręczny',
-  version: '2.2.51',
+  version: '2.2.52',
   description: 'Elity, Elity II, Herosi, Kolosi i Tytani wraz z przedmiotami, trasami i kalkulatorem łupu.',
   icon: 'B',
 
